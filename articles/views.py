d121d55e111cb32dd6a66d9b1602a61c66703ca3
AlexanderS68 <80540025+AlexanderS68@users.noreply.github.com>
2021-08-29 15:31:46 -0400

    article search not done

--- articles/views.py
+++ articles/views.py
@@ -18,22 +18,38 @@ configuration.api_key['X-AYLIEN-NewsAPI-Application-Key'] = '047069c3a217672289a
 client = aylien_news_api.ApiClient(configuration)
 api_instance = aylien_news_api.DefaultApi(client)
 
-# params = {
-#   'language': ['en'],
-#   'title': 'startup',
-#   'published_at_start': 'NOW-1HOUR',
-#   'published_at_end': 'NOW',
-#   'cursor': '*',
-#   'sort_by': 'published_at'
-# }
-
-
 
 def fetchPayload():
     
-    sectorName = ['Energy', 'Finance', 'Material', 'Industrial', 'Utility', 'Healthcare', 'Consumer Discretionary', 'IT', 'Communication services']
+    #This is for the categories
+    sectorName = ['Energy', 'Finance', 'Material', 'Industrial', 'Utility', 'Healthcare', 'Consumer Discretionary', 'IT', 'Communication services', 'Real Estate']
+
     sectorCategories = [['04005000', '04005001', '06003000', '06001000','06006009', '04005010', '04005004', '04005004'],['04008004', '04019000', '04016017', '04000000', '04006000', '04008023', '04016019'],['04011000', '04012005', '04012002'], ['04015001', '03006000', '04011002'], ['06001000',
-    '06006000','04005008','06005002','06010000','04005006','04005003','04005004', '04005005'], ['07013000', '07000000', '04002006', '14008000', '04002001'],['04007000', '04016055', '04008014', '13020000', '01000000'], ['04003000', '13000000', '13010000', '13017000'], ['01026002', '04003007', '04007009', '04010010']]
+    '06006000','04005008','06005002','06010000','04005006','04005003','04005004', '04005005'], ['07013000', '07000000', '04002006', '14008000', '04002001'],['04007000', '04016055', '04008014', '13020000', '01000000'], ['04003000', '13000000', '13010000', '13017000'], ['01026002', '04003007', '04007009', '04010010'], ['04008022', '11016002', '04004002', '04001003']]
+
+    #Placeholder for when article search is fleshed out. Similar to above
+    #
+
+    
+    payload = {
+        'title':'energy',
+        'published_at_start':'NOW-1DAY',
+        'categories_confident': 'True',
+    }
+
+    api_response_articles = api_instance.list_stories(**payload)
+
+    for ix, articleReturn in enumerate(api_response_articles.stories):
+            Article.objects.create(
+                title = articleReturn.title,
+                description = articleReturn.categories[0].label,
+                content = articleReturn.body,
+                url = articleReturn.source.home_page_url,
+                publishedAt = articleReturn.published_at,
+                sourceName = articleReturn.source.name,
+                sentiment = articleReturn.sentiment.body.polarity)
+
+
     for i in range(10):
         opts = { 
         'categories_taxonomy':'iptc-subjectcode',
@@ -42,10 +58,11 @@ def fetchPayload():
         'published_at_end':'NOW',
         'categories_confident': True
         }
+  
         try: 
             # List time series
             api_response = api_instance.list_time_series(**opts)
-            #pprint(api_response)
+            
         except ApiException as e:
             pprint("Exception when calling DefaultApi->list_time_series: %s\n" % e)
 
@@ -56,9 +73,11 @@ def fetchPayload():
                 count =  categoryDateSeries.count,
                 published_at = categoryDateSeries.published_at,
             )
+        
+        
     
 class Category_view_set(viewsets.ModelViewSet):
-    queryset = Article.objects.all()
+    queryset = Category.objects.all()
     serializer_class = Article_serializer
     permission_classes = [AllowAny]
 
@@ -74,7 +93,7 @@ class Category_view_set(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class Article_view_set(viewsets.ModelViewSet):
-
+    queryset = Article.objects.all()
     serializer_class = Article_serializer
     permission_classes = [AllowAny]
 
